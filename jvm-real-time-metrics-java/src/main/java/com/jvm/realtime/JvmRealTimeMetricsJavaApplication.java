777dbbafdaca80c7fba260740e7d102445f5a821
mmckeaveney <martinmckeaveney@gmail.com>
2015-12-03 19:06:22 +0000

    Initial Commit

---
+++ jvm-real-time-metrics-java/src/main/java/com/jvm/realtime/JvmRealTimeMetricsJavaApplication.java
@@ -0,0 +1,12 @@
+package com.jvm.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmRealTimeMetricsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmRealTimeMetricsJavaApplication.class, args);
+    }
+}
