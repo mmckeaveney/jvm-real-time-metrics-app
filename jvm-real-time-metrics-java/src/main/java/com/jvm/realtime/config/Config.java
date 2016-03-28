70014f7807d0f4d65be99099fdc0489af11f6522
mmckeaveney <martinmckeaveney@gmail.com>
2016-03-28 23:44:57 +0100

    fixed merge problem and added amazon SDK dependency

--- jvm-real-time-metrics-java/src/main/java/com/jvm/realtime/config/Config.java
+++ jvm-real-time-metrics-java/src/main/java/com/jvm/realtime/config/Config.java
@@ -2,6 +2,7 @@ package com.jvm.realtime.config;
 
 import com.jvm.realtime.client.JvmrtExceptionHandler;
 import com.jvm.realtime.data.RealTimeDataClient;
+import com.jvm.realtime.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,11 +18,19 @@ public class Config {
 
     @Autowired
     private RealTimeDataClient realTimeDataClient;
+    @Autowired
+    private EmailService emailService;
 
 //    @Bean
 //    public Filter authFilter() {
 //        return new OAuthWebFilter();
 //    }
+
+    @Bean
+    public String sendEmailTest() {
+       emailService.sendEmail();
+        return "Email sent";
+    }
 
 
 
